7332ee2c737af48fc750bce1edd519fd824f75ee
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-05-22 14:19:30 +0300

    feat: add header component to hompe page

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logoImage from "../assets/transpoint-logo.png";
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="left">
+        <img src={logoImage} />
+      </div>
+      <div className="right">
+        <p>Sell Property</p>
+        <p>Rent Property</p>
+        <p>Other Services</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
