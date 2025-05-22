252df601f9585ae20aacdb7977ca1f77ee22faae
V1n-T3ch <vincegichomo704@gmail.com>
2025-05-22 17:47:14 +0300

    Auth Pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../styles/App.css";
 import Home from "../pages/Home";
 import UploadCatalogue from "./Upload";
-import "../styles/App.css";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<h1>404 Not Found</h1>}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/upload" element={<UploadCatalogue />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   );
